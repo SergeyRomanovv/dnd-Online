fd2725de1d29975a17199d217458e3c2f7679f2f
Sergey Romanov <study-front@mail.ru>
2022-04-12 09:26:17 +0300

    auth work

--- client/src/redux/action/authAC.js
+++ client/src/redux/action/authAC.js
@@ -8,10 +8,28 @@ export const clearRegisterInputs = (data) => {
   return { type: 'CLEAR_INPUTS', payload: {} };
 };
 
-export const submitRegister = (dataInputs) => async(dispatch) =>{
-  const response = await axios.post('http://localhost:3001/auth/registration', dataInputs, {withCredentials: true});
+export const submitSignin = (dataInputs, type) => async(dispatch) =>{
+  const response = await axios.post(`http://localhost:3001/auth/${type}`, dataInputs, {withCredentials: true});
   const user = response.data.user.userName;
   localStorage.setItem('token', response.data.accessToken);
   dispatch(setUser(user));
   dispatch(clearRegisterInputs());
+};
+
+export const submitLogout = () => async(dispatch) =>{
+  const response = await axios.get(`http://localhost:3001/auth/logout`, {withCredentials: true});
+  localStorage.removeItem('token');
+  dispatch(setUser(''));
+  dispatch(clearRegisterInputs());
+};
+
+export const checkAuth = () => async (dispatch) => {
+  try {
+    const response = await axios.get(`http://localhost:3001/auth/refresh`, {withCredentials: true});
+    console.log('checkAuth',response);
+    localStorage.setItem('token', response.data.accessToken);
+    dispatch(setUser(response.data.user.userName));
+  } catch (err) {
+    console.log(err);
+  } 
 };
