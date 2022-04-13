b1704411dd503f51c917de02c6c37141bd7f9495
arturohanyan1 <arturohanyan100@mail.ru>
2022-04-13 21:12:59 +0400

    set attr, del attr, move attr

--- client/src/components/GamePage/GamePage.jsx
+++ client/src/components/GamePage/GamePage.jsx
@@ -12,6 +12,7 @@ export default function GamePage() {
   const [allBoards, setAllBoards] = useState([]);
   const [oneGameBoard, setOneGameBoard] = useState([]);
   const [gameBoardCoordinates, setGameBoardCoordinates] = useState({});
+  const [moveAttr, setMoveAttr ] = useState({});
   const [imgSrc, setImgSrc] = useState('');
 
   useEffect(() => {
@@ -25,19 +26,42 @@ export default function GamePage() {
   const getGameHundler = (id) => {
     console.log(id);
     const game = JSON.parse(allBoards.filter(el => el.id === id)[0].board);
-    dispatch({type: 'SET_ONE_GAME', payload: game})
+    dispatch({ type: 'SET_ONE_GAME', payload: game })
     // setOneGameBoard(game)
   }
 
-  function getTDHandler(e) {
-    const x = e.target.parentNode.rowIndex;
-    const y = e.target.cellIndex; 
-    setGameBoardCoordinates({ x, y });
-    console.log('xy', x, y, imgSrc);
-    dispatch({ type: 'SET_ATTR', payload: {x, y, imgSrc}});
+  function setTDHandler(e) {
+    console.log(1555555555555555555551);
   }
 
-  function getImgSrcHundler (e) {
+  function masterHandler(e) {
+    if (false) {
+      console.log('zzzz');
+      const x = e.target.parentNode.parentNode.rowIndex;
+      const y = e.target.parentNode.cellIndex;
+      setGameBoardCoordinates({ x, y });
+      console.log('doubleclick x y', x, y, imgSrc);
+      dispatch({ type: 'DEL_ATTR', payload: { x, y, imgSrc: '' } });
+    } else if (e.shiftKey) {
+      const x = e.target.parentNode.rowIndex;
+      const y = e.target.cellIndex;
+      setGameBoardCoordinates({ x, y });
+      console.log('xy', x, y, imgSrc);
+      dispatch({ type: 'SET_ATTR', payload: { x, y, imgSrc } });
+    } else if (e.altKey) {
+      console.log(11155);
+      const x = e.target.parentNode.parentNode.rowIndex;
+      const y = e.target.parentNode.cellIndex;
+      const imgSrc = e.target.alt;
+      setMoveAttr({ x, y, imgSrc });
+      // dispatch({ type: 'DEL_ATTR', payload: { x, y, imgSrc: '' } });
+    } else if (e.ctrlKey) {
+      console.log(moveAttr);
+      console.log(gameBoardCoordinates);
+    }
+  }
+
+  function getImgSrcHundler(e) {
     setImgSrc(e.target.alt);
   }
 
@@ -57,11 +81,11 @@ export default function GamePage() {
         <div className={style.mainSide}>
           <p>main gamePage</p>
           <div className={style.gameBox}>
-            <table className={style.tableBox} onClick={(e) => getTDHandler(e)}>
+            <table className={style.tableBox} onClick={(e) => masterHandler(e)} onDoubleClick={() => setTDHandler}>
               <thead></thead>
               <tbody>
-                { gamee.length ?
-                  gamee.map(e => <tr>{e.map(el => <td className={style.bgImg} style={{ backgroundImage: `url(${el.bgImg})` }}>{el.attr ? <img src={el.attr} alt="dnd online" style={{backgroundColor: '#ffffff00', width: '65px'}} /> : <span></span>}</td>)}</tr>) :
+                {gamee.length ?
+                  gamee.map(e => <tr>{e.map(el => <td tabindex="0" className={style.bgImg} style={{ backgroundImage: `url(${el.bgImg})` }}>{el.attr ? <img src={el.attr} alt={el.attr} style={{ backgroundColor: '#ffffff00', width: '65px' }} /> : <span></span>}</td>)}</tr>) :
                   <span>Chosse a game from left side</span>
                 }
               </tbody>
@@ -73,9 +97,9 @@ export default function GamePage() {
       </div>
       <div className={style.footerPanel}>
         <span>bottom panel</span>
-        <img src="./images/items/Bonefire1.png" alt="./images/items/Bonefire1.png" style={{width: '60px'}} onClick={getImgSrcHundler} />
-        <img src="./images/items/Elf2.png" alt="./images/items/Elf2.png" style={{width: '60px'}} onClick={getImgSrcHundler} />
-        <img src="./images/items/Chest1.png" alt="./images/items/Chest1.png" style={{width: '60px'}} onClick={getImgSrcHundler} />
+        <img src="./images/items/Bonefire1.png" alt="./images/items/Bonefire1.png" style={{ width: '60px' }} onClick={getImgSrcHundler} />
+        <img src="./images/items/Elf2.png" alt="./images/items/Elf2.png" style={{ width: '60px' }} onClick={getImgSrcHundler} />
+        <img src="./images/items/Chest1.png" alt="./images/items/Chest1.png" style={{ width: '60px' }} onClick={getImgSrcHundler} />
       </div>
     </>
   )
