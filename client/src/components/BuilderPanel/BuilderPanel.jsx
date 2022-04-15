fc347f8c45b5701a4eb6977a2fc65a35f4023428
arturohanyan1 <arturohanyan100@mail.ru>
2022-04-15 19:31:27 +0400

    get images by category

--- client/src/components/BuilderPanel/BuilderPanel.jsx
+++ client/src/components/BuilderPanel/BuilderPanel.jsx
@@ -7,18 +7,41 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function BuilderPanel() {
-  const [blackItems, setBlockItems] = useState([]);
+  // const [blackItems, setBlockItems] = useState([]);
   const gameMap = useSelector((store) => store.gameMap);
   const boardtitle = useSelector((store) => store.boardtitle);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  //proverks
+  const [categories, setCategories] = useState([]);
+  const [images, setImages ] = useState([]);
+  const [togle, setTogle] = useState({view: style.footerPanel1, icon: 'fa-solid fa-chevron-down'});
+  const [imgToggle, setImgToggle] = useState(style.panelImages);
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:3001/builder')
+  //     .then((res) => {
+  //       setBlockItems(res.data)
+  //     })
+  // }, []);
+
+  // proverks useffects end
   useEffect(() => {
-    axios.get('http://localhost:3001/builder')
+    axios.get('http://localhost:3001/builder/categories')
       .then((res) => {
-        setBlockItems(res.data)
+        // console.log(res.data)
+        setCategories(res.data);
       })
   }, []);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/builder/images`)
+      .then((res) => {
+        setImages(res.data);
+      })
+  }, []);
+  // proverks useeffect end
 
   const ttt = useSelector(state => state.tempImg);
 
@@ -42,22 +65,61 @@ export default function BuilderPanel() {
     dispatch({ type: 'SET_BOARD_TITLE', payload: { [ev.target.name]: ev.target.value } });
   }
 
+  
+ // proverks 
+  const getImagesHundler = (id) => {
+    setImgToggle(false)
+    console.log(id);
+    axios.post(`http://localhost:3001/builder/images/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setImgToggle(style.panelImages)
+        setImages(res.data);
+      })
+      
+      
+  }
+
+  function togleHundler() {
+    if (togle.view === style.footerPanel) {
+      setTogle({view: style.footerPanel1, icon: 'fa-solid fa-chevron-down'});
+    } else {
+      setTogle({view: style.footerPanel, icon: 'fa-solid fa-chevron-up'});
+    }
+  }
+
+  // proverks end
+
+
   return (
+    <div className={togle.view}>
+    <div>
+      <button onClick={togleHundler} className={style.toggleBtn}><span className={style.iconText}>Builder Panel</span> <i class={togle.icon}></i></button>
+    </div>
     <div className={style.panel}>
-      {
+      {/* {
         blackItems.map(el => (
           <div key={el.id} className={style.panelImages}>
             <img src={el.url} alt={el.url} tabindex="0" onClick={getSrcHundler} />
             <div className={style.title}>{el.title}</div>
           </div>
         ))
-      }
+      } */}
+      <div className={style.mainPanel}>
+        <div className={style.buttonsPanel}>
+          {categories.map(cat => <button id={cat.id} key={cat.id} onClick={() => getImagesHundler(cat.id)} >{cat.title}</button> )}
+        </div>
+        <div className={style.imagesPanel}>
+          {imgToggle ? images.map(pic => <img className={imgToggle} src={pic.url} alt={pic.url} tabindex="0" onClick={getSrcHundler} /> ) : <div className={style.emptyDiv}></div> }
+        </div>
+      </div>
       <div className={style.btnBox}>
 
         <input type="text" name='title' value={boardtitle.title || ''} onChange={titleHandler} />
         <button className={style.btn} onClick={saveHandler}>SAVE</button>
 
       </div>
+    </div>
     </div>
   )
 }
--- server/routes/builderRouter.js
+++ server/routes/builderRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Block, GameBoard } = require('../db/models')
+const { Block, GameBoard, Category } = require('../db/models')
 
 router.get("/", async (req, res) => {
   try {
@@ -27,6 +27,41 @@ router.post('/save', async (req, res) => {
     console.log(err);
     res.sendStatus(500);
   }
-})
+});
+
+router.get('/categories', async (req, res) => {
+  try {
+    const categories = await Category.findAll({ raw: true })
+    console.log(categories);
+    res.json(categories);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+router.get('/images', async (req, res) => {
+  try {
+    const images = await Block.findAll({ where: { category_id: 1 }, raw: true });
+    console.log(images);
+    res.json(images);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+router.post('/images/:id', async (req, res) => {
+  try {
+    const category_id = req.params.id;
+    console.log(category_id);
+    const images = await Block.findAll({ where: { category_id }, raw: true });
+    console.log(images);
+    res.json(images);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
 module.exports = router
